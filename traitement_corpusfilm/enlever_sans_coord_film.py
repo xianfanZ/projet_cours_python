9ac8224bd66a332891fe7bb0f5cd0453ab44234f
WANG Yizhe <wangyizhe0201@gmail.com>
2016-12-08 11:37:19 +0100

    modéliser le corpus film

---
+++ traitement_corpusfilm/enlever_sans_coord_film.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from lxml import etree
+file = "/Users/wangyizhe/Desktop/projetPy/film_changed.xml"
+tree = etree.parse("/Users/wangyizhe/Desktop/projetPy/film2011.xml")
+node_geos = tree.xpath("/tournagesdefilmsparis2011/film/geo_coordinates")
+for node_geo in node_geos:
+	if node_geo.text == None:
+		parent_node = node_geo.getparent()
+		print (parent_node)
+		parent_node.clear()
+tree.write(file)
