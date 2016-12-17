a5e17fef59ff5cf6e84d03910c88c1dc1c4e3f05
WANG Yizhe <wangyizhe0201@gmail.com>
2016-12-17 21:54:34 +0100

    film_json

---
+++ script/geojson_film.py
@@ -0,0 +1,40 @@
+import json
+from lxml import etree
+
+#def transfer_xml_json():
+
+def write_in_file_json(list_json):
+	with open("film.json", "w") as fo:
+		content = "{"+str(list_json) +"}"
+		fo.write(content)
+
+def json_film(file_name):
+	tree = etree.parse(file_name)
+	nodes_film = tree.xpath("/tournagesdefilmsparis2011/film")
+	list_json = []
+	for node_film in nodes_film:
+		dic_film = {}
+		for child in node_film.getchildren():
+			json_child = {}
+			child_tag = child.tag
+			chilld_text = child.text
+			dic_film[child_tag] = chilld_text
+		list_json.append(dic_film)
+	#write_in_file_json(list_json)
+#json_film("/Users/wangyizhe/Desktop/projetPy/film_final.xml")
+
+
+
+def geo_json():
+
+	geo_json = {
+	"type" : "Circle",
+	"coordinates": coor,
+	"radius" : 0.5,
+	"properties": properties
+	}
+	print geo_json
+
+def parser_json():
+	f = file("/Users/wangyizhe/Desktop/projetPy/film.json")
+	
