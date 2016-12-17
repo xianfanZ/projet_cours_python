c97aa6b1f0dacdee05cb9986e59f48176f8ad46e
Nidia Hernández <nidiahernandez@Nidias-MacBook-Air.local>
2016-12-17 23:37:39 +0100

    geocoordonnées monuments

--- script/traite_monuments.py
+++ script/traite_monuments.py
@@ -4,7 +4,7 @@
 # Traitement des fichiers des geocoordonnées des monuments de la France pour obtenir les coordonnées 
 # des monuments parisiens choisis
 
-from lxml import etree
+from geopy.geocoders import Nominatim
 
 def my_monuments_list(nomfichier):
 	"""
@@ -18,55 +18,31 @@ def my_monuments_list(nomfichier):
 			my_monuments.append(ligne)
 	return my_monuments
 
-def extract_coord_1(listemon, fichiermon): # pas finie. Parsing avec module?
+def extract_coord(listemon):
 	"""
-	Traitement fichier json geocoordonnées des monuments de la France.
 	Extraction des coordonnées des monuments recherchés.
-	args : liste des monuments choisis, nom fichier coordonnées json
+	arg : liste des monuments choisis
+	Retourne fichier geojson de monuments choisis et geocoordonnées 
 	"""
-	with open("monuments-nettoye.json", "w") as sortie:
-		entree = open(fichiermon, "r").read()
-		entree = entree.replace("\\u00e9", "é")
-		entree = entree.replace("\\u00e8", "è")
-		entree = entree.replace("\\u00f4", "ô")
-		entree = entree.replace("\\u00c9", "É")
-		entree = entree.replace("\\u00e2", "â")
-		sortie.write(entree)
-	#for element in listemon:
-	#	if element in entree:
-	#		print(element)
-	sortie.close()
-
-def extract_coord_2(listemon, fichiermon2): # Finalement, cette fonction ne sert à rien parce que  
-											# le fichier monuments.odt ne contient qu'un seul des monuments recherchés
-	"""
-	Traitement deuxième fichier geocoordonnées des monuments de la France.
-	Extraction des coordonnées des monuments recherchés.
-	args: liste des monuments choisis, content.xml issu du fichier odt des monuments
-	"""
-	tree = etree.parse(fichiermon2)
-	# on utilise xpath pour recuperer le contenu textuel du fichier
-	elements = tree.xpath("//office:body/office:spreadsheet/table:table/table:table-row/table:table-cell/text:p",
-                  namespaces={"office":"urn:oasis:names:tc:opendocument:xmlns:office:1.0",
-                              "table": "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
-                              "text":"urn:oasis:names:tc:opendocument:xmlns:text:1.0"})
-	
-	els= [] # contenu textuel de chaque cellule
-	for elem in elements:
-		els.append(elem.text)
-
-	for el in els:
-		if el in listemon: 
-			lat = els.index(el)+1
-			lon = els.index(el)+2
-			print(el, els[lat], els[lon])
-
+	with open("../json/monuments_coord.geojson", "w") as sortie:
+		sortie.write( "{\"type\":\"FeatureCollection\",\"features:\"\n\n[\n\n")
+		geolocator = Nominatim()
+		for monument in listemon:
+			#location = geolocator.geocode(monument, geometry="geojson") La sortie geojson de geopy est trop verbeuse et rend des coordonnées de polygones
+			#print(location.raw)
+			location = geolocator.geocode(monument)
+			sortie.write( "{\"type\": \"Feature\",\n" )
+			sortie.write( " \"geometry\": {\n" )
+			sortie.write( "   \"type\": \"Point\",\n" )
+			sortie.write( "   \"coordinates\": "+ str([location.latitude, location.longitude] )+"\n },\n" )
+			sortie.write( " \"properties: {\n" )
+			sortie.write( "   \"name\": \""+monument+"\"\n }\n},\n\n" )
+		sortie.write("]")
 
 
 def main():
-	monuments = my_monuments_list("liste_de_monuments.txt")
-	extract_coord_1(monuments, "monuments.json")
-	#extract_coord_2(monuments, "content.xml")
+	monuments = my_monuments_list("../data/liste_de_monuments.txt")
+	extract_coord(monuments)
 
 if __name__ == '__main__':
     main()
