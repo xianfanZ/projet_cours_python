1d5a325a165625afb8aacabe5b3973a2fc8d1427
Xianfan ZHANG <zhangxianfan.cheryl@gmail.com>
2017-01-04 22:04:02 +0100

    ...

--- script/tableau_resultat.py
+++ script/tableau_resultat.py
@@ -2,7 +2,7 @@
 
 from lxml import etree
 from collections import OrderedDict
-import traite_monuments
+import traite_monuments,math
 
 tree = etree.parse("../xml/filter_coord_films_new.xml")
 dico_monuments = {}
@@ -16,8 +16,8 @@ somme_films = sum(dico_monuments.values())
 sortie = "<!DOCTYPE html SYSTEM \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
 sortie += "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
 sortie += "\t<head><title>Tableau récapitulatif</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"./css/table.css\"/></head>\n"
-sortie += "\t<body><div class=\"ficheResult\"><h2>Fiche récapitulative des résultats obtenus</h2><table class=\"table_result\">\n"
-sortie += "\t\t<tr><th>Espace</th><th>monument</th><th>nb tournages</th><th>pourcentage</th><th>préféré par genre</th></tr>\n"
+sortie += "\t<body><div class=\"ficheResult\" align=\"center\"><h2>Fiche récapitulative des résultats obtenus</h2><table class=\"table_result\">\n"
+sortie += "\t\t<tr><th>Espace</th><th>monument</th><th>nb tournages</th><th>pourcentage</th></tr>\n"
 
 
 for niv in dico_espace_monuments:
@@ -25,13 +25,13 @@ for niv in dico_espace_monuments:
     lignespan = len(list_monuments)
     print(niv,lignespan)
     if niv == "standard":
-        sortie += "\t\t\t<tr><td rowspan=\"{}\">113411</td>".format(lignespan)
+        sortie += "\t\t\t<tr><td rowspan=\"{}\">{}m<sup>2</sup></td>".format(lignespan,int(10*19**2*math.pi))
     elif niv == "<+>":
-        sortie += "\t\t\t<tr><td rowspan=\"{}\">255175</td>".format(lignespan)
+        sortie += "\t\t\t<tr><td rowspan=\"{}\">{}m<sup>2</sup></td>".format(lignespan,int(15*19**2*math.pi))
     elif niv == "<++>":
-        sortie += "\t\t\t<tr><td rowspan=\"{}\">355175</td>".format(lignespan)
+        sortie += "\t\t\t<tr><td rowspan=\"{}\">{}m<sup>2</sup></td>".format(lignespan,int(20*19**2*math.pi))
     else:
-        sortie += "\t\t\t<tr><td rowspan=\"{}\">555175</td>".format(lignespan)
+        sortie += "\t\t\t<tr><td rowspan=\"{}\">{}m<sup>2</sup></td>".format(lignespan,int(25*19**2*math.pi))
     cpt = 1
     for monument in list_monuments:
         if monument in dico_monuments:
