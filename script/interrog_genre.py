a09b8bed1aabf3f759ea03c742bc66e9caa26e11
Xianfan ZHANG <zhangxianfan.cheryl@gmail.com>
2017-01-08 23:38:42 +0100

    update

--- script/interrog_genre.py
+++ script/interrog_genre.py
@@ -33,9 +33,27 @@ def mk_dico_monument_genre(fichier):
 	#print(mon_gp["Centre Pompidou"])
 	return mon_gp
 
+def mk_dico_genre_prefere(fichier):
+	"""
+	Construit dictionnaire de genres préférés par monument.
+	:param fichier: ../xml/films_genre_pays.xml
+	:return: dico {monument: genre préféré}
+	"""
+	dico_monument_genre = mk_dico_monument_genre(fichier)
+	dico_genre_prefere = {}
+	for key in dico_monument_genre:
+		sorted_k = sorted(dico_monument_genre[key], key=(dico_monument_genre[key]).get, reverse=True)
+		try:
+			sorted_k[0] == "N/A"
+			dico_genre_prefere[key] = sorted_k[1]
+		except:
+			dico_genre_prefere[key] = sorted_k[0]
+	return dico_genre_prefere
+
+
 
 def main():
-	dico = mk_dico_monument_genre("/Users/nidiahernandez/Desktop/projet_cours_python/xml/films_genre_pays.xml")
+	dico = mk_dico_monument_genre("../xml/films_genre_pays.xml")
 	for key in dico:
 		sorted_k = sorted(dico[key], key=(dico[key]).get, reverse=True)
 		print("Monument : {}".format(key))
@@ -44,6 +62,7 @@ def main():
 			print("Genre péféré: {}".format(sorted_k[1]))
 		except:
 			print("Genre péféré: {}".format(sorted_k[0]))
+
 
 if __name__ == '__main__':
 	main()
--- script/tableau_resultat.py
+++ script/tableau_resultat.py
@@ -7,22 +7,24 @@
 
 from lxml import etree
 from collections import OrderedDict
-import traite_monuments,math
+import traite_monuments,math,interrog_genre
 
 tree = etree.parse("../xml/filter_coord_films_new.xml")
 dico_monuments = {}
 dico_espace_monuments = traite_monuments.my_monuments_dico("../data/liste_de_monuments.txt")
+dico_genre_prefere = interrog_genre.mk_dico_genre_prefere("../xml/films_genre_pays.xml")
 
-for elem in tree.iter(tag='Momument_proche'):
+for elem in tree.iter(tag='Monument_proche'):
     dico_monuments[elem.text] = dico_monuments.get(elem.text,0)+1
 dico_monuments = OrderedDict(sorted(dico_monuments.items(), key=lambda t: t[1],reverse=True))
 somme_films = sum(dico_monuments.values())
+
 
 sortie = "<!DOCTYPE html SYSTEM \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
 sortie += "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
 sortie += "\t<head><title>Tableau récapitulatif</title><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"./css/table.css\"/></head>\n"
 sortie += "\t<body><div class=\"ficheResult\" align=\"center\"><h2>Fiche récapitulative des résultats obtenus</h2><table class=\"table_result\">\n"
-sortie += "\t\t<tr><th>Espace</th><th>monument</th><th>nb tournages</th><th>pourcentage</th></tr>\n"
+sortie += "\t\t<tr><th>Espace</th><th>monument</th><th>nb tournages</th><th>pourcentage</th><th>genre préféré</th></tr>\n"
 
 
 for niv in dico_espace_monuments:
@@ -39,17 +41,23 @@ for niv in dico_espace_monuments:
         sortie += "\t\t\t<tr><td rowspan=\"{}\">{}m<sup>2</sup></td>".format(lignespan,int(25*19**2*math.pi))
     cpt = 1
     for monument in list_monuments:
+        if monument in dico_genre_prefere:
+            genre = dico_genre_prefere[monument]
+            #print(monument+":"+genre)
+        else:
+            genre = ""
+            #print(monument+":"+genre)
         if monument in dico_monuments:
             pourcentage = dico_monuments[monument]/somme_films
             if cpt == 1:
-                sortie += "<td>{}</td><td>{}</td><td>{:.2%}</td></tr>\n".format(monument,dico_monuments[monument],pourcentage)
+                sortie += "<td>{}</td><td>{}</td><td>{:.2%}</td><td>{}</td></tr>\n".format(monument,dico_monuments[monument],pourcentage,genre)
             else:
-                sortie += "\t\t\t<tr><td>{}</td><td>{}</td><td>{:.2%}</td></tr>\n".format(monument,dico_monuments[monument],pourcentage)
+                sortie += "\t\t\t<tr><td>{}</td><td>{}</td><td>{:.2%}</td><td>{}</td></tr>\n".format(monument,dico_monuments[monument],pourcentage,genre)
         else:
-            sortie += "\t\t\t<tr><td>{}</td><td>{}</td><td></td></tr>\n".format(monument,0)
+            sortie += "\t\t\t<tr><td>{}</td><td>{}</td><td></td><td>{}</td></tr>\n".format(monument,0,genre)
         cpt += 1
 sortie += "\t</table></div></body></html>"
 
-with open("../web/tableu.html","w") as f:
+with open("../web/tableau.html","w") as f:
     f.write(sortie)
 
