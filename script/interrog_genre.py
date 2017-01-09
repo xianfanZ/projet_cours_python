4e883b49b172d505afbe97a8c8486d522856f1b5
Nidia Hernández <nidiahernandez@Nidias-MacBook-Air.local>
2017-01-09 18:24:52 +0100

    update

--- script/interrog_genre.py
+++ script/interrog_genre.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #python3
 
-# Interroger les données
+# Obtention du genre préféré par monument
 # Usage : python3 interrog.py
 # Installer 
-# Données source : film_genre_pays.xml
+# Données source : ../xml/film_genre_pays.xml
 # Résultat : genre préféré par monument
 
 from lxml import etree
@@ -33,35 +33,31 @@ def mk_dico_monument_genre(fichier):
 	#print(mon_gp["Centre Pompidou"])
 	return mon_gp
 
-def mk_dico_genre_prefere(fichier):
+def mk_dico_genre_prefere(dico_monument_genre):
 	"""
 	Construit dictionnaire de genres préférés par monument.
-	:param fichier: ../xml/films_genre_pays.xml
-	:return: dico {monument: genre préféré}
+	param: dictionnaire {monument: {genre, nº tournages}
+	return: dico {monument: genre préféré}
 	"""
-	dico_monument_genre = mk_dico_monument_genre(fichier)
 	dico_genre_prefere = {}
 	for key in dico_monument_genre:
 		sorted_k = sorted(dico_monument_genre[key], key=(dico_monument_genre[key]).get, reverse=True)
-		try:
-			sorted_k[0] == "N/A"
+		if sorted_k[0] == "N/A":
 			dico_genre_prefere[key] = sorted_k[1]
-		except:
+		else:
 			dico_genre_prefere[key] = sorted_k[0]
 	return dico_genre_prefere
 
 
 
 def main():
+	print("Calcul du nombre de tournages de chaque genre par monument...")
 	dico = mk_dico_monument_genre("../xml/films_genre_pays.xml")
-	for key in dico:
-		sorted_k = sorted(dico[key], key=(dico[key]).get, reverse=True)
-		print("Monument : {}".format(key))
-		try:
-			sorted_k[0] == "N/A"
-			print("Genre péféré: {}".format(sorted_k[1]))
-		except:
-			print("Genre péféré: {}".format(sorted_k[0]))
+	print("Genre préféré par monument :\n")
+	dico2 = mk_dico_genre_prefere(dico)
+	for key2 in dico2:
+		print("{} {}\n".format(key2, dico2[key2]) )
+
 
 
 if __name__ == '__main__':
